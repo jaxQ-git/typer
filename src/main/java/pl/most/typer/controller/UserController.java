0965603749e22de8672dffd52c7e7635aff93255
Michal.Ostrega <michalostrega@gmail.com>
2020-04-03 13:35:01 +0200

    UserController

--- src/main/java/pl/most/typer/controller/UserController.java
+++ src/main/java/pl/most/typer/controller/UserController.java
@@ -1,4 +1,30 @@
 package pl.most.typer.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import pl.most.typer.repository.UserRepository;
+import pl.most.typer.service.CustomUserDetailsService;
+
+@RequestMapping("/rest/hello")
+@RestController
 public class UserController {
+
+    private CustomUserDetailsService customUserDetailsService;
+    private UserRepository userRepository;
+
+    public UserController(CustomUserDetailsService customUserDetailsService, UserRepository userRepository) {
+        this.customUserDetailsService = customUserDetailsService;
+        this.userRepository = userRepository;
+    }
+
+    @GetMapping("/all")
+    public String hello() {
+        return "Hello";
+    }
+
+    @GetMapping("/secured/all")
+    public String securedHello() {
+        return "Secured Hello";
+    }
 }
