0c322ae0ab5532a69cb5cf63dcad61b9e94cc5ff
Michal.Ostrega <michalostrega@gmail.com>
2020-04-06 09:07:01 +0200

    Blocked the same user name or mail user registration

--- src/main/java/pl/most/typer/repository/UserRepository.java
+++ src/main/java/pl/most/typer/repository/UserRepository.java
@@ -1,12 +1,13 @@
 package pl.most.typer.repository;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import pl.most.typer.model.User;
 
 import java.util.Optional;
 
-public interface UserRepository extends CrudRepository<User,Long> {
-
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByUsername(String username);
 
     Optional<User> findByToken(String token);
